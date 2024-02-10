285ec936130528e2589e7eb6e860c797f8e6ca67
zomvr <zomvroficial@gmail.com>
2024-02-10 18:25:11 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const axios = require("axios");
+const Fuse = require("fuse.js");
+
+const app = express();
+const port = 3000 || process.env.PORT;
+const baseUrl = "https://raw.githubusercontent.com/zomvr2/free-exercise-db/main";
+
+app.get("/", (req, res) => {
+  res.send("All working fine!");
+});
+
+// For specific exercise by id, Ex: /exercise/1
+app.get("/exercise/:id", async (req, res) => {
+  const url = `${baseUrl}/dist/exercises.json`;
+  const id = req.params.id;
+
+  try {
+    const response = await axios.get(url);
+    const exercises = response.data;
+
+    const exercise = exercises.find((exercise) => exercise.id === id);
+
+    if (!exercise) {
+      res.status(404).send("Exercise not found");
+      return;
+    }
+
+    res.send(exercise);
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+});
+
+// For searching exercises, Ex: /search?name=bench&page=1
+app.get("/search", async (req, res) => {
+  const url = `${baseUrl}/dist/exercises.json`;
+  const name = req.query.name;
+  const page = req.query.page || 1;
+
+  try {
+    const response = await axios.get(url);
+    const exercises = response.data;
+
+    const fuse = new Fuse(exercises, {
+      keys: ["name"],
+    });
+
+    const result = fuse.search(name);
+    const start = (page - 1) * 10;
+    const end = start + 10;
+    const data = result.slice(start, end);
+
+    res.json({
+      results: result.length,
+      page: parseInt(page),
+      total_pages: Math.ceil(result.length / 10),
+      data,
+    });
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
